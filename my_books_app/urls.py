e37d5ca02c900bc7daa72146b72ca39862d0c4f8
adam-weiler <adam.weiler@yahoo.ca>
2019-07-30 12:10:28 -0400

    Adding edit, update, delete functions to app.

--- my_books_app/urls.py
+++ my_books_app/urls.py
@@ -21,5 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('books/', views.all),
-    path('books/<int:id>', views.show)
+    # path('books/<str:something>', views.something), #This line is problematic. It would run before new, create.
+    path('books/new', views.new), #This line is new.
+    path('books/create', views.create), #This line is new.
+    path('books/<int:id>/update', views.update), #This line is new.
+    path('books/<int:id>', views.show),
+    path('books/<int:id>/edit', views.edit), #This line is new.
+    path('books/<int:id>/delete', views.delete), #This line is new.
 ]
--- my_books_app/views.py
+++ my_books_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render #Render means you don't need the HttpResponse, or HttpResponseRedirect.
 from django.http import HttpResponseRedirect
 from my_books_app.models import *
+# import pdb #This line is new
 
 def index(request):
     return HttpResponseRedirect('/books')
@@ -16,3 +17,47 @@ def show(request, id): #Passing in the ID in the URL address.
     our_book = Book.objects.get(pk=id)
     context = {'our_book': our_book}
     return render(request, 'show.html', context)
+
+
+def new(request): #This function is new.
+    return render(request, 'new.html')
+
+def create(request): #This function is new.
+    # breakpoint() #Then run request.POST
+    new_book = Book()
+    new_book.title = request.POST["title"]
+    new_book.author = request.POST["author"]
+    new_book.summary = request.POST["summary"]
+    new_book.save()
+
+    return HttpResponseRedirect('/books')
+    #The data has been submitted.
+    #We could send them to thank you page.
+    #Or send back to a list of books
+
+def edit(request, id): #This function is new.
+    our_book = Book.objects.get(pk=id) #Getting info from database, to prepopulate the form.
+    context = {"book": our_book}
+
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    our_book = Book.objects.get(pk=id) #Getting info from database, to update it's data.
+    our_book.title = request.POST["title"]
+    our_book.author = request.POST["author"]
+    our_book.summary = request.POST["summary"]
+    our_book.save()
+
+    return HttpResponseRedirect('/books')
+    #The data has been updated.
+    #We could send them to thank you page.
+    #Or send back to a list of books
+
+def delete(request, id):
+    our_book = Book.objects.get(pk=id) #Getting info from database, to update it's data.
+    our_book.delete()
+
+    return HttpResponseRedirect('/books')
+    #The data has been updated.
+    #We could send them to thank you page.
+    #Or send back to a list of books
